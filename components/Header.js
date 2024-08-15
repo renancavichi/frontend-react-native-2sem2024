e54a4a12529120a5fae4776c0b25ac7b2e7d4cd8
Renan Cavichi <renancavichi@gmail.com>
2024-08-15 15:55:37 -0300

    add expo vector-icons

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,18 @@
 import { View, Text, StyleSheet} from 'react-native'
+import { Image } from 'expo-image'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 export default function Header(){
     return (
         <View style={styles.header}>
-          <Text>Header Ok</Text>
+            <View style={styles.user}>
+                <Image 
+                    style={styles.avatar}
+                    source="https://avatars.githubusercontent.com/u/4259630?v=4"
+                />
+                <Text style={styles.name}>Renan Cavichi</Text>
+            </View>
+            <Ionicons style={styles.menu} name="menu" size={24} color="black" />
         </View>
     )
 }
@@ -11,8 +20,27 @@ export default function Header(){
 const styles = StyleSheet.create({
     header: {
         flex: 1,
-        backgroundColor: "#899986",
-        justifyContent: 'center',
-        alignItems: 'center'
+        //backgroundColor: "#899986",
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    user: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10
+    },
+    avatar: {
+        width: 50,
+        height: 50,
+        borderRadius: 25
+    },
+    name: {
+        fontWeight: '600',
+        fontSize: 18
+    },
+    menu: {
+        padding: 10
     }
 })
