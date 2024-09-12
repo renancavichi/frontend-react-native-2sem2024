deff7fbc511a661c360908ff69ea740dd0309495
Renan Cavichi <renancavichi@gmail.com>
2024-09-12 16:00:17 -0300

    add create account

--- components/Content.js
+++ components/Content.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import {View, StyleSheet, Text} from 'react-native'
+import {View, StyleSheet, Text, TextInput, Pressable} from 'react-native'
 import CardAccount from './CardAccount'
 //import CardAccount2 from './CardAccount2'
 //import Calc from './Calc'
@@ -7,6 +7,10 @@ import CardAccount from './CardAccount'
 export default function Content(){
 
   const [accounts, setAccounts] = useState([])
+  const [txtServico, setTxtServico] = useState('')
+  const [txtUsername, setTxtUsername] = useState('')
+  const [txtPass, setTxtPass] = useState('')
+  const [txtImgUrl, setTxtImgUrl] = useState('')
  
    useEffect(() => {
         const getAccounts = async () => {
@@ -24,11 +28,74 @@ export default function Content(){
         getAccounts()
    }, [])
 
+   const handleCreateAccount = async () => {
+    const account = {
+        service: txtServico,
+        username: txtUsername,
+        logo_image: txtImgUrl,
+        pass: txtPass,
+        user_id: 1
+    }
+
+    const response = await fetch('http://localhost:3000/account', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(account)
+    })
+      if(response.ok){
+        const data = await response.json()
+        console.log(data)
+        setAccounts([data.account, ...accounts])
+        return
+      }
+      console.log('Erro ao carregar accounts')
+      return
+   }
 
     return (
         <View style={styles.content}>
         
         {/* <Calc /> */}
+
+        <View>
+          <Text>Serviço:</Text>
+          <TextInput 
+            style={styles.input}
+            onChangeText={setTxtServico}
+            value={txtServico}
+            placeholder='Digite o nome do serviço...'
+            placeholderTextColor='#DDDDDD'
+          />
+          <Text>Username:</Text>
+          <TextInput 
+            style={styles.input}
+            onChangeText={setTxtUsername}
+            value={txtUsername}
+          />
+          <Text>Password:</Text>
+          <TextInput 
+            style={styles.input}
+            onChangeText={setTxtPass}
+            value={txtPass}
+          />
+          <Text>Logo URL:</Text>
+          <TextInput 
+            style={styles.input}
+            onChangeText={setTxtImgUrl}
+            value={txtImgUrl}
+            keyboardType='url'
+          />
+          <Pressable
+            style={styles.button}
+            onPress={handleCreateAccount}
+          >
+            <Text>Cadatrar</Text>
+          </Pressable>
+
+
+        </View>        
 
         { accounts.length === 0 && <Text>Loading...</Text>}
 
@@ -53,5 +120,21 @@ const styles = StyleSheet.create({
         padding: 15
         //justifyContent: 'center',
         //alignItems: 'center'
-      }
+      },
+    input: {
+      borderWidth: 1,
+      borderStyle: 'solid',
+      borderColor: '#444444',
+      paddingHorizontal: 10,
+      paddingVertical: 6,
+      marginVertical: 5,
+      borderRadius: 5
+    },
+    button: ({pressed}) => [{
+      backgroundColor: pressed ? '#ed7900': '#f97f01',
+      alignItems: 'center',
+      marginVertical: 10,
+      borderRadius: 10,
+      paddingVertical: 5
+    }]
 })
